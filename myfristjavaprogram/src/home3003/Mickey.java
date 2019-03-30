3f10ff727cc0917a19e7d428e1fd2f5ab582164c
Mitul <minalp999@gmail.com>
2019-03-30 17:36:31 +0000

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/Mickey.java
@@ -0,0 +1,26 @@
+package home3003;//variable declair
+
+public class Mickey {
+
+    public static void main(String[] args) {
+        int b = 123;
+        float c = 20.8f;
+        char e = 'k';
+        double h = 78.70d;
+        boolean s = true;
+        short g = -12;
+        long k = 1234567;
+        byte a = 67;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(h);
+        System.out.println(s);
+        System.out.println(g);
+        System.out.println(k);
+        System.out.println(a);
+    }
+
+}
+
+
