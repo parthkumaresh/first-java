42a0265055a094ffdd6f439e3c7fa604165f3200
Mitul <minalp999@gmail.com>
2019-04-02 10:44:52 +0100

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/WeekDays_6.java
@@ -0,0 +1,45 @@
+package home3003;
+
+import java.util.Scanner;
+
+public class WeekDays_6 {
+    public static void main(String[] args) {
+        //  Scanner names = new Scanner(System.in );
+        // System.out.println( " Enter the names of the days: " );
+        int day;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter day number between 1 and 7 : ");
+
+        day = sc.nextInt();
+
+        switch (day) {
+
+
+                case 1:
+                    System.out.println("1st Day of week is Monday");
+                    break;
+                case 2:
+                    System.out.println("2nd Day of week is Tuesday");
+                    break;
+                case 3:
+                    System.out.println("3rd Day of week is Wednesday");
+                    break;
+                case 4:
+                    System.out.println("4th Day of week is Thursday");
+                    break;
+                case 5:
+                    System.out.println("5th Day of week is Friday");
+                    break;
+                case 6:
+                    System.out.println("6th Day of week is Saturday");
+                    break;
+                case 7:
+                    System.out.println("7th Day of week is Sunday");
+                    break;
+                default:
+                    System.out.println("Week contains 1 to 7 days only");
+                    break;
+            }
+        }
+    }
