245ac6ffa02b924a9a88a712badf40335a2638b5
Mitul <minalp999@gmail.com>
2019-04-04 22:01:29 +0100

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/NumberSymbol_14.java
@@ -0,0 +1,25 @@
+package home3003;
+
+import java.util.Scanner;
+
+public class NumberSymbol_14 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\"WRITE ANY ALPHABET, NUMBER OR SYMBOL\"");
+        char char1 = scanner.next().charAt(0);
+
+        if (char1 >= 48 && char1 <= 57) {
+            System.out.print("\"IT IS DIGIT\"");
+
+        } else if ((char1 >= 'a' && char1 <= 'z') || (char1 >= 'A' && char1 <= 'Z')) {
+            System.out.print("\"IT IS ALPHABET\"");
+
+        } else {
+            System.out.print("\"IT IS SYMBOL\"");
+        }
+    }
+}
