76767c449e86203e288753356f48a816293418b5
Mitul <minalp999@gmail.com>
2019-04-04 21:59:21 +0100

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/CityNameIfElse_10.java
@@ -0,0 +1,49 @@
+package home3003;
+
+import java.util.Scanner;
+
+public class CityNameIfElse_10 {
+    public static void main(String[] args) {
+
+
+        Scanner intp = new Scanner(System.in);
+
+        System.out.print("Enter city name: ");
+
+        char cityName;
+
+        cityName = (char) intp.next().charAt(0);
+
+        if (cityName == 'a'){
+
+            System.out.println( "\nAthens ");}
+
+        else if (cityName == 'b'){
+
+            System.out.println("\nBarcelona");}
+
+        else if (cityName == 'c'){
+
+            System.out.println("\nCardiff");}
+
+        else if (cityName == 'd'){
+
+            System.out.println("\nDover");}
+
+        else if (cityName == 'e'){
+
+            System.out.println("\nElizabeth");}
+
+        else if (cityName == 'f'){
+
+            System.out.println("Florence");}
+
+        else
+
+            System.out.println("Invalid City Name:  ");
+    }
+
+    }
+
+
+
