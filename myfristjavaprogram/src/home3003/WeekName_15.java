c56aec2b8db2afc4a24da843db49632f8750eaff
Mitul <minalp999@gmail.com>
2019-04-04 22:03:27 +0100

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/WeekName_15.java
@@ -0,0 +1,44 @@
+package home3003;
+
+import java.util.Scanner;
+
+public class WeekName_15 {
+
+    public static void main(String[] args) {
+
+        int day;
+        Scanner days = new Scanner(System.in);
+
+        System.out.print("Enter day number between 1 and 7 : ");
+
+        day = days.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println(" It is Monday on 7th Day of week ");
+                break;
+            case 2:
+                System.out.println(" It is Tuesday on 7th Day of week ");
+                break;
+            case 3:
+                System.out.println(" It is Wednesday on 7th Day of week ");
+                break;
+            case 4:
+                System.out.println(" It is Thursday on 7th Day of week ");
+                break;
+            case 5:
+                System.out.println(" It is Friday on 7th Day of week ");
+                break;
+            case 6:
+                System.out.println(" It is Saturday on 7th Day of week ");
+                break;
+            case 7:
+                System.out.println(" Sunday is 7th Day of week ");
+                break;
+            default:
+                System.out.println("Week contains 1 to 7 days only");
+                break;
+        }
+    }
+
+}
