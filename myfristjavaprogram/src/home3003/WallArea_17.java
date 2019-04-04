3fea519fd64e50d0f678d5869f2aed409f6ff9ed
Mitul <minalp999@gmail.com>
2019-04-04 22:03:02 +0100

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/WallArea_17.java
@@ -0,0 +1,62 @@
+package home3003;
+
+public class WallArea_17 {
+
+
+    double width;
+    double height;
+
+    public WallArea_17() {
+
+    }
+
+    public WallArea_17(double width, double height) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getArea() {
+        return getHeight() * getWidth();
+    }
+
+    public static void main(String[] args) {
+        WallArea_17 wall = new WallArea_17(5, 4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+}
