909f1889f42a75a8c7850f1e886fcd689c65a36f
Mitul <minalp999@gmail.com>
2019-04-04 08:15:43 +0100

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/OddEven_8.java
@@ -0,0 +1,24 @@
+package home3003;
+
+import java.util.Scanner;
+
+public class OddEven_8 {
+
+        public static void main(String[] args) {
+            int oddeven;
+
+
+            System.out.println("\nEnter \"DIGIT\" to check if it is \"ODD\" or \"EVEN\"\n");
+
+            Scanner in = new Scanner(System.in);
+            oddeven = in.nextInt();
+
+            if (oddeven % 2 == 0)
+
+                System.out.println("This number is \"EVEN NUMBER\"  ");
+
+            else
+                System.out.println("This number is \"ODD NUMBER\"");
+        }
+    }
+
