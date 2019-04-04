9bacda0b722a396133e7358f82b448e83593cdd1
Mitul <minalp999@gmail.com>
2019-04-04 08:17:11 +0100

    My Frist Practice commit

---
+++ myfristjavaprogram/src/home3003/Calculator_12.java
@@ -0,0 +1,46 @@
+package home3003;
+
+import java.util.Scanner;
+
+public class Calculator_12 {
+        public static void main(String args[])
+        {
+            int a, b, res;
+            char ch;
+            Scanner scan = new Scanner(System.in);
+
+            System.out.print("Enter First Number : ");
+            a = scan.nextInt();
+            System.out.print("Enter Second Number : ");
+            b = scan.nextInt();
+            System.out.print("Enter Operator (+, -, *, /) : ");
+            ch = scan.next().charAt(0);
+
+            if(ch == '+')
+            {
+                res = a + b;
+                System.out.print("Result = " +res);
+            }
+            else if(ch == '-')
+            {
+                res = a - b;
+                System.out.print("Result = " +res);
+            }
+            else if(ch == '*')
+            {
+                res = a * b;
+                System.out.print("Result = " +res);
+            }
+            else if(ch == '/')
+            {
+                res = a / b;
+                System.out.print("Result = " +res);
+            }
+            else
+            {
+                System.out.print("Wrong Operator !!!");
+            }
+
+        }
+    }
+
