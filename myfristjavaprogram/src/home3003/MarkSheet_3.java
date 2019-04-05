78448b1804136ba4d07978b90b56a050128dc44e
Mitul <minalp999@gmail.com>
2019-04-05 11:53:05 +0100

    My Frist Practice commit

--- myfristjavaprogram/src/home3003/MarkSheet_3.java
+++ myfristjavaprogram/src/home3003/MarkSheet_3.java
@@ -21,5 +21,48 @@ public class MarkSheet_3 {
         System.out.println( "  Total " + sum);
         double average =  sum/3;
         System.out.println( " Average " + average );
+
+        System.out.println("|-------------------------------------------|");
+        System.out.println("|                \"Marksheet\"               |");
+        System.out.println("|-------------------------------------------|");
+        System.out.println("| Name       :       "+str+"                     |");
+        System.out.println("| Rollno     :       " +a+"                     |" );
+        System.out.println("|----------------------------------------|");
+        System.out.println("|Subjects    :      Marks                   |");
+        System.out.println("|-------------------------------------------|");
+        System.out.println("| Maths      :      " +b+"                    |");
+        System.out.println("| English    :      " +c+"                  |");
+        System.out.println("| Science    :      " +d+"                  |");
+        System.out.println("|-------------------------------------------------|");
+        System.out.println("| Total      :       "+ sum+"                       |");
+        System.out.println("| Percentage :       " +average+"%                      |");
+
+
+
+        if (average>35) {
+            System.out.println("\n|pass/fail:     \"Pass\"                       |\n");
+        }
+        else {
+            System.out.println("\n|pass/fail :   \"Fail\"                       |\n");
+        }
+        if (average>=80) {
+            System.out.println("|Grade     :     \"A+\"                     |\n");
+        }
+        else if (average>=60){
+            System.out.println("|Grade      :     \"A\"                     |\n");
+        }
+        else if (average>=50){
+
+            System.out.println("|Grade      :     \"B\"                    |\n");
+        }
+        else if (average>=35) {
+
+            System.out.println("|Grade      :      \"C\"                    |\n");
+        }
+        else if (average<35){
+            System.out.println("|Grade       :      \"D\"                    |\n");
+        }
+        System.out.println("|----------------------------------------------------|\n");
+
     }
 }
